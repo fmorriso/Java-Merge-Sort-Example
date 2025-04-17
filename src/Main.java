8dc642449625d1d91e70f36c1ec993ce27daecd1
Fred Morrison <frederick.morrison@outlook.com>
2025-04-17 12:44:37 -0600

    add example Merge Sort class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    // Driver method
+    public static void main(String args[])
+    {
+        int a[] = { 12, 11, 13, 5, 6, 7 };
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Calling of Merge Sort
+        MergeSort ob = new MergeSort();
+        ob.sort(a, 0, a.length - 1);
+
+        int n = a.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(a[i] + " ");
     }
 }
